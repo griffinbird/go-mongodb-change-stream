ca82b7b77e16a95de883badc4767d87cc6a20d1e
griffinbird <benito.griffin@gmail.com>
2022-09-06 13:36:23 +1000

    Added showing Request Charges and query execution time when using Cosmos DB Mongo API.

--- main.go
+++ main.go
@@ -13,12 +13,13 @@ import (
 
 	"github.com/joho/godotenv"
 
-	//"github.com/gobwas/glob/util/strings"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var IsMongoAtlas bool = false
 
 type DbEvent struct {
 	DocumentKey   documentKey `bson:"documentKey"`
@@ -34,11 +35,150 @@ type result struct {
 	Brand      string             `bson:"brand"`
 }
 
+type requestStatistics struct {
+	ActivityID                                  string
+	CommandName                                 string
+	EstimatedDelayFromRateLimitingInMilliseconds int
+	RequestCharge                               float64
+	RequestDurationInMilliseconds               int
+	RetriedDueToRateLimiting                    bool
+	OK                                          int
+}
+
+func main() {
+	// waitGroup to wait for all goroutines launched here to finish
+	var waitGroup sync.WaitGroup
+
+	// Set client options and connect to MongoDB
+	err := godotenv.Load(".env")
+		if err != nil {
+			log.Printf("error loading .env file")
+		}
+	
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.TODO(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// check if using Mongo Atlas or Cosmos DB API for Mongo DB
+	pattern := "mongodb.net"
+	connStr := os.Getenv("MONGODB_URI")
+	str_pos := strings.Index(connStr, pattern)
+	if (str_pos > -1) {
+		fmt.Printf("Connected to Mongo Atlas\n\n")
+		IsMongoAtlas = true
+	} else {
+		fmt.Printf("Connected to Cosmos DB Mongo API\n\n")
+		IsMongoAtlas = false
+	}
+
+	// Cleanup the connection when main function exists
+	defer client.Disconnect(context.TODO())
+
+	// set Mongodb database and collection name
+	database := client.Database("change-stream-demo")
+	collection := database.Collection("cart")
+
+	// This will watch all any and all changes to the documents within the collection
+	// and will be later used to iterate over indefinately
+	//streamOptions := options.ChangeStream().SetFullDocument(options.UpdateLookup) <- this won't work with CDB.
+	pipeline := mongo.Pipeline{
+		bson.D{{Key: "$match", Value: bson.D{{Key: "operationType", Value: bson.D{{Key: "$in", Value: []string{"update", "insert", "replace"}}}}}}},
+		bson.D{{Key: "$project", Value: bson.D{
+            {Key: "_id", Value: 1},
+            {Key: "documentKey", Value: 1},        
+            {Key: "fullDocument.userID", Value: 1},
+            {Key: "fullDocument.itemType", Value: 1},
+            {Key: "fullDocument.brand", Value: 1},
+        }},
+		},
+	}
+	//stream, err := collection.Watch(context.TODO(), mongo.Pipeline{}) <- this won't work with CDB.
+	stream, err := collection.Watch(context.TODO(), pipeline, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+	if err != nil {
+		panic(err)
+	}
+
+	// Waitgroup counter
+	waitGroup.Add(1)
+
+	routineCtx, cancelFn := context.WithCancel(context.Background())
+	_ = cancelFn
+
+	// Watches collection in database and prints out any changed document
+	// go-routine to make code non-blocking
+	go listenToDBChangeStream(client, routineCtx, waitGroup, stream, collection, database)
+
+	// Insert a MongoDB record every 5 seconds
+	go insertRecord(client, database, collection)
+
+	// Block until the WaitGroup counter goes back to 0; all the workers notified they’re done.
+	waitGroup.Wait()
+}
+
+// function to insert data records to MongoDB collection
+func insertRecord(client *mongo.Client, database *mongo.Database, collection *mongo.Collection) {
+	// pre-populated values for ItemType and Brand
+	ItemType := make([]string, 0)
+	ItemType = append(
+		ItemType,
+		"T-Shirts",
+		"Shoes",
+		"Pants",
+		"Socks",
+		"Belt",
+		"Watch",
+		"Bracelet",
+	)
+	Brand := make([]string, 0)
+	Brand = append(Brand, "Nike", "Addidas", "Puma", "Asics", "Mizuno")
+
+	// insert new records to MongoDB every 5 seconds
+	for {
+		item := result{
+			ID:         primitive.NewObjectID(),
+			UserID:     strconv.Itoa(rand.Intn(10000)),
+			ItemType: ItemType[rand.Intn(len(ItemType))],
+			Brand:  Brand[rand.Intn(len(Brand))],
+		}
+		_, err := collection.InsertOne(context.TODO(), item)
+		if err != nil {
+			log.Fatal(err)
+		}
+		requestChargeValue := ""
+		if !IsMongoAtlas {
+			statistics, err := GetLastRequestStats(client, database)
+			requestChargeValue = fmt.Sprintf("RequestCharge: %.2f %dms\n ", statistics.RequestCharge, statistics.RequestDurationInMilliseconds)
+			if err != nil {
+				log.Fatalf("%v",err)
+				//return err
+			}
+			fmt.Printf("%v" + "   " + "%v", item, requestChargeValue)
+		} else {
+			fmt.Printf("%v\n", item)	
+		}
+		time.Sleep(3 * time.Second)
+	}
+}
+
 func listenToDBChangeStream(
+	client *mongo.Client,
 	routineCtx context.Context,
 	waitGroup sync.WaitGroup,
 	stream *mongo.ChangeStream,
 	collection *mongo.Collection,
+	database *mongo.Database,
 ) {
 	// Cleanup defer functions when this function exits
 	defer stream.Close(routineCtx)
@@ -79,120 +219,35 @@ func listenToDBChangeStream(
 			if writeErr != nil {
 				log.Fatal(writeErr)
 			}
-			// Print the changed document in JSON format
-			fmt.Println(string(data))
-			fmt.Println("")
+			requestChargeValue := ""
+			if !IsMongoAtlas {
+				statistics, err := GetLastRequestStats(client, database)
+				requestChargeValue = fmt.Sprintf("RequestCharge: %.2f %dms\n ", statistics.RequestCharge, statistics.RequestDurationInMilliseconds)
+				if err != nil {
+					log.Fatalf("%v",err)
+					//return err
+				}
+				// Print the changed document in JSON format with Request Charge value
+				fmt.Print(string(data) + "   ")
+				fmt.Print(string(requestChargeValue))
+				fmt.Println("")
+			} else {
+				// Print the changed document in JSON format
+				fmt.Print(string(data))
+				fmt.Println("\n")
+			}
 		}
 	}
 }
 
-func main() {
-	// waitGroup to wait for all goroutines launched here to finish
-	var waitGroup sync.WaitGroup
+func GetLastRequestStats(client *mongo.Client, database *mongo.Database) (*requestStatistics, error) {
+	ctx := context.TODO()
 
-	// Set client options and connect to MongoDB
-	err := godotenv.Load(".env")
-		if err != nil {
-			log.Printf("error loading .env file")
-		}
-	client, err := mongo.Connect(
-		context.TODO(),
-		options.Client().ApplyURI(os.Getenv("MONGODB_URI")),
-	)
+	statistics := requestStatistics{}
+	map1 := map[string]interface{}{"getLastRequestStatistics": "1"}
+	err := database.RunCommand(ctx, map1, nil).Decode(&statistics)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// check if using Mongo Atlas or Cosmos DB API for Mongo DB
-	pattern := "mongodb.net"
-	connStr := os.Getenv("MONGODB_URI")
-	str_pos := strings.Index(connStr, pattern)
-	if (str_pos > -1) {
-		fmt.Printf("Connected to Mongo Atlas\n")
-	} else {
-		fmt.Printf("Connected to Cosmos DB Mongo API\n")
-	}
-
-	// Cleanup the connection when main function exists
-	defer client.Disconnect(context.TODO())
-
-	// set Mongodb database and collection name
-	database := client.Database("change-stream-demo")
-	collection := database.Collection("cart")
-
-	// This will watch all any and all changes to the documents within the collection
-	// and will be later used to iterate over indefinately
-	//streamOptions := options.ChangeStream().SetFullDocument(options.UpdateLookup) <- this won't work with CDB.
-	pipeline := mongo.Pipeline{
-		bson.D{{Key: "$match", Value: bson.D{{Key: "operationType", Value: bson.D{{Key: "$in", Value: []string{"update", "insert", "replace"}}}}}}},
-		bson.D{{Key: "$project", Value: bson.D{
-            {Key: "_id", Value: 1},
-            {Key: "documentKey", Value: 1},        
-            {Key: "fullDocument.userID", Value: 1},
-            {Key: "fullDocument.itemType", Value: 1},
-            {Key: "fullDocument.brand", Value: 1},
-        }},
-		},
-	}
-	//stream, err := collection.Watch(context.TODO(), mongo.Pipeline{}) <- this won't work with CDB.
-	stream, err := collection.Watch(context.TODO(), pipeline, options.ChangeStream().SetFullDocument(options.UpdateLookup))
-	if err != nil {
-		panic(err)
-	}
-
-	// Waitgroup counter
-	waitGroup.Add(1)
-
-	routineCtx, cancelFn := context.WithCancel(context.Background())
-	_ = cancelFn
-
-	// Watches collection in database and prints out any changed document
-	// go-routine to make code non-blocking
-	go listenToDBChangeStream(routineCtx, waitGroup, stream, collection)
-
-	// Insert a MongoDB record every 5 seconds
-	go insertRecord(collection)
-
-	// Block until the WaitGroup counter goes back to 0; all the workers notified they’re done.
-	waitGroup.Wait()
-}
-
-// function to insert data records to MongoDB collection
-func insertRecord(collection *mongo.Collection) {
-	// pre-populated values for ItemType and Brand
-	ItemType := make([]string, 0)
-	ItemType = append(
-		ItemType,
-		"T-Shirts",
-		"Shoes",
-		"Pants",
-		"Socks",
-		"Belt",
-		"Watch",
-		"Bracelet",
-	)
-	Brand := make([]string, 0)
-	Brand = append(Brand, "Nike", "Addidas", "Puma", "Asics", "Mizuno")
-
-	// insert new records to MongoDB every 5 seconds
-	for {
-		item := result{
-			ID:         primitive.NewObjectID(),
-			UserID:     strconv.Itoa(rand.Intn(10000)),
-			ItemType: ItemType[rand.Intn(len(ItemType))],
-			Brand:  Brand[rand.Intn(len(Brand))],
-		}
-		_, err := collection.InsertOne(context.TODO(), item)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%v\n", item)
-		time.Sleep(5 * time.Second)
-	}
+	return &statistics, nil
 }
